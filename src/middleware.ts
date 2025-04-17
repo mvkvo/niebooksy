25ba6df137a9e7a2ffa0f42bea8eb5e2ab50d3da
Adrian Chalimoniuk <adrian.chalimoniuk@reckitt.com>
2025-04-17 13:40:28 +0200

    init

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,22 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    const isLoggedIn = !!req.nextauth.token;
+    const isOnDashboard = req.nextUrl.pathname.startsWith("/dashboard");
+
+    if (isOnDashboard && !isLoggedIn) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = {
-  // specify the route you want to protect
   matcher: ["/dashboard/:path*"],
 };
